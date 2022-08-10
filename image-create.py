2f06fcd963aedafc6ca3ee3581c8b0040445342d
ardayaldiz07 <ardayaldiz07@gmail.com>
2022-08-10 18:08:10 +0300

    first commit

---
+++ image-create.py
@@ -0,0 +1,26 @@
+from PIL import Image, ImageDraw, ImageFont
+
+font = "Poppins-Black.ttf"
+fontSize = 100
+words = ["Arda", "Umut", "Hafize", "Ayşe"]
+
+
+def imageCreate(text):
+
+    image = Image.open("bg-layer.jpg")
+    font_type = ImageFont.truetype(font=font, size=fontSize)
+    # image = image.resize((1920,1080))
+    draw = ImageDraw.Draw(image)
+    text_width, text_height = draw.textsize(text, font_type)
+
+    x = (image.width/2) - (text_width/2)
+    y = (image.height/2) - (text_height)
+
+    draw.text(xy=(x, y), text=text.upper(),
+              fill=(255, 255, 255), font=font_type)
+
+    image.save("output/"+text.lower()+".jpg", quality=50)
+
+
+for word in words:
+    imageCreate(word)
